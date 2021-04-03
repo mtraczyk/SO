35bba45ee417b36802cef41c7c6224441218d080
Michał Traczyk <mt418401@students.mimuw.edu.pl>
2021-04-03 11:15:09 +0200

    Some changes

--- compile.sh
+++ compile.sh
@@ -2,6 +2,6 @@
 
 N=10;
 
-gcc -DN=$N -c -Wall -Wextra -O2 -std=c11 -o unit_test.o unit_test.c
+gcc -DN=$N -c -Wall -Wextra -O2 -std=c11 -o example.o example.c
 nasm -DN=$N -f elf64 -w+all -w+error -o notec.o notec.asm
-gcc notec.o unit_test.o -lpthread -o example
+gcc notec.o example.o -lpthread -o example
